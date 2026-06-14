a84513a98fa08257f67086dd0436eeefbc0ee29d
Kevin Loughead <kvnloughead@gmail.com>
2026-06-14 15:06:29 -0400

    allow CORs from localhost, all ports

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const { PORT } = require('./config');
-const authRouter = require('./routes/auth');
-const recipesRouter = require('./routes/recipes');
-const usersRouter = require('./routes/users');
+const express = require("express");
+const cors = require("cors");
+const { PORT } = require("./config");
+const authRouter = require("./routes/auth");
+const recipesRouter = require("./routes/recipes");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: /^http:\/\/localhost(:\d+)?$/ }));
 app.use(express.json());
 
-app.use('/auth', authRouter);
-app.use('/recipes', recipesRouter);
-app.use('/users', usersRouter);
+app.use("/auth", authRouter);
+app.use("/recipes", recipesRouter);
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.listen(PORT, () => {
